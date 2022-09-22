25fae89492f4f85badb9d171067b69fa9fe3c58c
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-09-22 11:51:22 +0200

    Ckpt

--- src/fdf_error_handler.c
+++ src/fdf_error_handler.c
@@ -6,7 +6,7 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 13:31:42 by segarcia          #+#    #+#             */
-/*   Updated: 2022/09/22 11:33:26 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/09/22 11:51:08 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	fd_validation(int fd, t_fdf *d)
 		if (d)
 			free_all(d);
 		perror("Error");
-		system("leaks fdf");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -29,7 +28,6 @@ void	argc_validation(int argc)
 	if (argc != 2)
 	{
 		ft_printf("Usage: ./fdf <filename>\n");
-		system("leaks fdf");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -37,7 +35,6 @@ void	argc_validation(int argc)
 void	map_format_error(void)
 {
 	ft_printf("Found wrong line length. Exiting.\n");
-	system("leaks fdf");
 	exit(EXIT_FAILURE);
 }
 
@@ -46,6 +43,5 @@ void	malloc_error(t_fdf *d)
 	if (d->mlx_ptr && d->win_ptr)
 		mlx_destroy_window(d->mlx_ptr, d->win_ptr);
 	ft_printf("Memory allocation error\n");
-	system("leaks fdf");
 	exit(EXIT_FAILURE);
 }
